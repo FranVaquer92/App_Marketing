da62fc8101a2d30a8c8fb095890dbf53bce36b03
Fran Vaquer <franciscofran92@gmail.com>
2021-07-01 12:21:58 +0200

    Update app_mkt.py

--- app_mkt.py
+++ app_mkt.py
@@ -24,15 +24,7 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="expanded",
      )
-st.markdown("""
-<style>
-body {
-  background: #ff0099; 
-  background: -webkit-linear-gradient(to right, #ff0099, #493240); 
-  background: linear-gradient(to right, #ff0099, #493240); 
-}
-</style>
-    """, unsafe_allow_html=True)
+st.markdown("<span style=“background-color:#121922”>",unsafe_allow_html=True)
 
 st.title('Segmentación y clasificación de clientes.')
 subido = 0
